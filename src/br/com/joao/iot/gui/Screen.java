e10c2eae733cbfd52abe23a99067deee5075a6ca
JoaoVictor_Moraes <joaovictor.mroaes2728@gmail.com>
2025-05-24 13:32:14 -0300

    introducing regex in code for validating of IP

--- src/br/com/joao/iot/gui/Screen.java
+++ src/br/com/joao/iot/gui/Screen.java
@@ -1,6 +1,5 @@
 package br.com.joao.iot.gui;
 
-import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Container;
 import java.awt.Font;
@@ -11,14 +10,13 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JList;
-import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextField;
 
 import br.com.joao.iot.model.Ip;
 
 public class Screen {
-	
+
 	// Variables of class
 	private JTextField textIp;
 	private JLabel labelIp;
@@ -26,10 +24,9 @@ public class Screen {
 	private JScrollPane scroll;
 	private JList listIp;
 	private JLabel labelError;
-	
-	
+
 	public void createScreen() {
-		
+
 		// Configures of screen
 		JFrame screen = new JFrame();
 		screen.setSize(500, 480);
@@ -37,83 +34,84 @@ public class Screen {
 		screen.setResizable(false);
 		screen.setLayout(null);
 		screen.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
+
 		// Creating container for add components and Font's for elements
 		Container container = screen.getContentPane();
-		Font fontError =  new Font("Arial" , Font.ITALIC , 25);
-		Font fontGeneral = new Font("Arial" , Font.PLAIN , 15);
-		Font fontList = new Font("Arial" , Font.PLAIN , 13);
-		
+		Font fontError = new Font("Arial", Font.ITALIC, 25);
+		Font fontGeneral = new Font("Arial", Font.PLAIN, 15);
+		Font fontList = new Font("Arial", Font.PLAIN, 13);
+
 		// Creating JLabel and determinate yours features
 		labelIp = new JLabel();
 		labelIp.setText("Insira o IP");
 		labelIp.setFont(fontGeneral);
 		labelIp.setHorizontalAlignment(JLabel.CENTER);
 		labelIp.setBounds(0, 20, 500, 30);
-		
+
 		// Creating JTextField and determinate yours features
 		textIp = new JTextField();
 		textIp.setFont(fontGeneral);
 		textIp.setBounds(175, 60, 150, 30);
-		
+
 		// Creating JButton and determinate yours features
 		buttonCalc = new JButton();
 		buttonCalc.setFont(fontGeneral);
 		buttonCalc.setText("Fornecer Dados");
 		buttonCalc.setBounds(175, 120, 150, 30);
-		
+
 		// Creating JList
 		listIp = new JList();
 		listIp.setFont(fontList);
-		
+
 		// Creating JScrollPane and determinate yours features
 		scroll = new JScrollPane(listIp);
 		scroll.setBounds(07, 180, 470, 120);
-		
+
 		// Creating JLabel error and determinate yours features
 		labelError = new JLabel();
 		labelError.setFont(fontError);
 		labelError.setForeground(Color.red);
 		labelError.setHorizontalAlignment(JLabel.CENTER);
-		labelError.setBounds(0, 325, 500, 40);;
-		
+		labelError.setBounds(0, 325, 500, 40);
+		;
+
 		// Creating Listener of Button
 		buttonCalc.addActionListener(new ActionListener() {
-			
-			// Anonymous function  
+
+			// Anonymous function
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				
+
 				labelError.setText("");
-				
+
 				Ip ip = new Ip();
-				
+
 				ip.setIp(textIp.getText());
 				ip.extractCidr();
 				ip.extractClas();
-				 
+
 				ip.defineIpClass();
-				
+
 				// Printing result in JList
 				String[] resultEnd = ip.vectorResult();
 				listIp.setListData(resultEnd);
-				
-				//If for print in screen the message of error
+
+				// If for print in screen the message of error
 				if (ip.getError().equals("x")) {
 					labelError.setText("Valor digitado inválido");
 				}
 			}
 		});
-		
+
 		// Add components in Screen
-		container.add(textIp);	
+		container.add(textIp);
 		container.add(labelIp);
 		container.add(buttonCalc);
 		container.add(scroll);
 		container.add(labelError);
-		
+
 		// Turned Screen visible
 		screen.setVisible(true);
 	}
-	
+
 }
--- src/br/com/joao/iot/model/Ip.java
+++ src/br/com/joao/iot/model/Ip.java
@@ -1,199 +1,219 @@
 package br.com.joao.iot.model;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Ip {
-	
-	// Variables of class 
+
+	// Variables of class
 	private String ip;
 	private String mask;
 	private String maskBinary;
 	private int ipQuantd;
 	private String cidr;
 	private String clas;
-	
+
 	// Variable utilized for determined case have a error
 	private String error = "";
 
 	// Method set of IP
 	public String setIp(String ip) {
+		
+		String firstRegex = "\\.(\\d+)\\.";
+		String secondRegex = "(?:\\d+\\.)(?:\\d+\\.)(\\d+)\\.";
+		String thirdRegex = "(?:\\d+\\.)(?:\\d+\\.)(?:\\d+\\.)(\\d+)\\.";
+		
+		
+		
+		Pattern firstPattern = Pattern.compile(firstRegex);
+		Matcher firstMatcher = firstPattern.matcher(ip);
+		
+		Pattern secondPattern = Pattern.compile(secondRegex);
+		Matcher secondMatcher = secondPattern.matcher(ip);
+		
+		Pattern thirdPattern = Pattern.compile(thirdRegex);
+		Matcher thirdMatcher = thirdPattern.matcher(ip);
+		
+		
+		if (firstMatcher.find()) {
+			System.out.println(firstMatcher.group(1));
+		} 
+		
+		if (secondMatcher.find()) {
+			System.out.println(secondMatcher.group(1));
+		}
+		
+		if (thirdMatcher.find()) {
+			System.out.printf("buscando...%s", thirdMatcher.group(1));
+		}else {
+			System.out.println("error");
+		}
 		this.ip = ip;
 		return ip;
 	}
-	
+
 	public String getError() {
 		return error;
 	}
-	
-	// Function for get the CIDR of IP 
+
+	// Function for get the CIDR of IP
 	public String extractCidr() {
 		cidr = ip.substring(ip.length() - 2);
 		return cidr;
 	}
-	
-	// Function for get the three first character of IP and get the class 
+
+	// Function for get the three first character of IP and get the class
 	public String extractClas() {
-		clas = ip.substring(0 , 3);
+		clas = ip.substring(0, 3);
 		return clas;
 	}
 	
-		public void defineIpClass() {
-			
-			// Variable contend the three first digits without "."
-			clas = clas.replace(".", "");
-			
-			// Block utilized for determine if entrance of data this correct 
-			try {
-				
-				// Variable responsible for transform of String for int  
-				int clasInt = Integer.parseInt(clas);
-				
-				// If and Else utilized for defined the class of IP  
-				if (clasInt >= 1 && clasInt <= 127) {
-					clas = "A";
-					
-				} else if (clasInt >= 128 && clasInt <= 191) {
-					clas = "B";
-				
-				} else if (clasInt >= 192 && clasInt <= 223) {
-					clas = "C";	
-				
-				} else if (clasInt >= 224 && clasInt <= 239) {
-					clas = "D";
-					
-				} else if (clasInt >= 240 && clasInt <= 255) {
-					clas = "E";
-					
-				}
-				
-				defineMaskAndMaskBinary();
-				 
-			} catch (Exception e) {
-				
-				error = "x";
-			}
-			
-			
-		}
+	public void defineIpClass() {
 
-		public void defineMaskAndMaskBinary() {
-			 
+		// Variable contend the three first digits without "."
+		clas = clas.replace(".", "");
+
+		// Block utilized for determine if entrance of data this correct
+		try {
+
 			// Variable responsible for transform of String for int
-			int cidrInt = Integer.parseInt(cidr);
-			int test = cidrInt;
-			
-			// Variable contend the keys binary for determine the sub-net  
-			int[] binary = {
-					128,
-					64,
-					32,
-					16,
-					8,
-					4,
-					2,
-					1
-			};
-			
-			// While utilized for extract the number multiplier of sub-net 
-			while (test >= 8) {
-				test-=8;
+			int clasInt = Integer.parseInt(clas);
+
+			// If and Else utilized for defined the class of IP
+			if (clasInt >= 1 && clasInt <= 127) {
+				clas = "A";
+
+			} else if (clasInt >= 128 && clasInt <= 191) {
+				clas = "B";
+
+			} else if (clasInt >= 192 && clasInt <= 223) {
+				clas = "C";
+
+			} else if (clasInt >= 224 && clasInt <= 239) {
+				clas = "D";
+
+			} else if (clasInt >= 240 && clasInt <= 255) {
+				clas = "E";
+
 			}
-			
-			int cidrDec = 0;
-			int begin = 0;
-			int i = 0;
-			
-			// If and Else for define if the mask will 255 or another number 
-			if (test > 0) {
-				
-				// While responsible for calculation of sub-mask 
-				while (begin < test) {
-					cidrDec += binary[i];
-					i++;
-					begin++;
-				}
-			} else {
-				cidrDec = 255;
-			
-			}
-				
-			int mask255 = 255;
-			String mask255Binary = Integer.toBinaryString(mask255);
-			String cidrBinary = Integer.toBinaryString(cidrDec);
-			
-			// If's and Else's that will assemble the mask in decimal and binary completely 
-			if (cidrInt <= 8) {
-				mask = cidrDec + "." + "0" + "." + "0" + "." + "0";
-				
-			} else if (cidrInt > 8 && cidrInt <= 16) {
-				mask = mask255 + "." + cidrDec + "." + "0" + "." + "0";
-				
-			} else if (cidrInt > 16 && cidrInt <= 24) {
-				mask = mask255 + "." + mask255 + "." + cidrDec + "." + "0";
-				
-			} else if (cidrInt > 24 && cidrInt <= 32) {
-				mask = mask255 + "." + mask255 + "." + mask255 + "." + cidrDec;
-				
-			}
-			
-			if (cidrInt <= 8) {
-				maskBinary = cidrBinary + " . " + "00000000" + " . " + "00000000" + " . " + "00000000";
-				
-			} else if (cidrInt > 8 && cidrInt <= 16) {
-				maskBinary = mask255Binary + " . " + cidrBinary + " . " + "00000000" + " . " + "00000000";
-				
-			} else if (cidrInt > 16 && cidrInt <= 24) {
-				maskBinary = mask255Binary + " . " + mask255Binary + " . " + cidrBinary + " . " + "00000000";
-				
-			} else if (cidrInt > 24 && cidrInt <= 32) {
-				maskBinary = mask255Binary + " . " + mask255Binary + " . " + mask255Binary + " . " + cidrBinary;
-				
-			}
-			
-			
-			calculateIp(cidrInt);
+
+			defineMaskAndMaskBinary();
+
+		} catch (Exception e) {
+
+			error = "x";
 		}
 
-		public void calculateIp(int cidr) {
-			
-			// Calculation for get the quantity of IP's available and conversation of Double for int
-			double quantdDouble = (Math.pow(2, 32 - cidr) - 2);
-			int quantdInt = (int)quantdDouble;
-			
-			if (quantdInt < 0) {
-				ipQuantd = 0;
-			} else {
-				ipQuantd = quantdInt;
-			}
-			
-			
-			
-		}
+	}
+
+	public void defineMaskAndMaskBinary() {
 		
-		public String[] vectorResult() {
-			
-			// Vector that will store all result and print in JList 
-			String[] result = null;
-			
-			if (error.equals("")) {
-				result = new String[] {
-				
-					"A classe do IP " + ip + " é: " + clas,
-					"",
-					"A máscara é: " + mask,
-					"",
-					"A quantidade de hosts disponíveis é igual a: " + ipQuantd,
-					"",
-					"A máscara deste IP em binário é: " + maskBinary
-				};
-				
-			} else {
-				result = new String[] {
-					
-					""	
-					
-				};
-			}
-			
-			return result;
+		// Variable responsible for transform of String for int
+		int cidrInt = Integer.parseInt(cidr);
+		int test = cidrInt;
+
+		if (cidrInt > 32) {
+			error = "x";
 		}
+
+		// Variable contend the keys binary for determine the sub-net
+		int[] binary = { 128, 64, 32, 16, 8, 4, 2, 1 };
+
+		// While utilized for extract the number multiplier of sub-net
+		while (test >= 8) {
+			test -= 8;
+		}
+
+		int cidrDec = 0;
+		int begin = 0;
+		int i = 0;
+
+		// If and Else for define if the mask will 255 or another number
+		if (test > 0) {
+
+			// While responsible for calculation of sub-mask
+			while (begin < test) {
+				cidrDec += binary[i];
+				i++;
+				begin++;
+			}
+		} else {
+			cidrDec = 255;
+
+		}
+
+		int mask255 = 255;
+		String mask255Binary = Integer.toBinaryString(mask255);
+		String cidrBinary = Integer.toBinaryString(cidrDec);
+
+		// If's and Else's that will assemble the mask in decimal and binary completely
+		if (cidrInt <= 8) {
+			mask = cidrDec + "." + "0" + "." + "0" + "." + "0";
+
+		} else if (cidrInt > 8 && cidrInt <= 16) {
+			mask = mask255 + "." + cidrDec + "." + "0" + "." + "0";
+
+		} else if (cidrInt > 16 && cidrInt <= 24) {
+			mask = mask255 + "." + mask255 + "." + cidrDec + "." + "0";
+
+		} else if (cidrInt > 24 && cidrInt <= 32) {
+			mask = mask255 + "." + mask255 + "." + mask255 + "." + cidrDec;
+
+		}
+
+		if (cidrInt <= 8) {
+			maskBinary = cidrBinary + " . " + "00000000" + " . " + "00000000" + " . " + "00000000";
+
+		} else if (cidrInt > 8 && cidrInt <= 16) {
+			maskBinary = mask255Binary + " . " + cidrBinary + " . " + "00000000" + " . " + "00000000";
+
+		} else if (cidrInt > 16 && cidrInt <= 24) {
+			maskBinary = mask255Binary + " . " + mask255Binary + " . " + cidrBinary + " . " + "00000000";
+
+		} else if (cidrInt > 24 && cidrInt <= 32) {
+			maskBinary = mask255Binary + " . " + mask255Binary + " . " + mask255Binary + " . " + cidrBinary;
+
+		}
+
+		calculateIp(cidrInt);
+	}
+
+	public void calculateIp(int cidr) {
+
+		// Calculation for get the quantity of IP's available and conversation of Double
+		// for int
+		double quantdDouble = (Math.pow(2, 32 - cidr) - 2);
+		int quantdInt = (int) quantdDouble;
+
+		if (quantdInt < 0) {
+			ipQuantd = 0;
+		} else {
+			ipQuantd = quantdInt;
+		}
+
+	}
+
+	public String[] vectorResult() {
+
+		// Vector that will store all result and print in JList
+		String[] result = null;
+
+		if (error.equals("")) {
+			result = new String[] {
+
+					"A classe do IP " + ip + " é: " + clas, "", "A máscara é: " + mask, "",
+					"A quantidade de hosts disponíveis é igual a: " + ipQuantd, "",
+					"A máscara deste IP em binário é: " + maskBinary };
+
+		} else {
+			result = new String[] {
+
+					""
+
+			};
+		}
+
+		return result;
+	}
 
 }
